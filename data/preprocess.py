402c3619a2a3d535a25da1f9758505087f2e3d0b
leon-huang <leon@njuopen.com>
2014-04-03 20:51:26 +0800

    add function date2int

--- data/preprocess.py
+++ data/preprocess.py
@@ -13,10 +13,12 @@ current_dir = os.path.split(os.path.abspath(__file__))[0]
 csv_data_path = os.path.join(current_dir, 't_alibaba_data.csv')
 raw_data_path = os.path.join(os.path.join(current_dir, 'raw_data.npy'))
 
+def date2int(date):
+    return (date - START_DATE).days
+
 def date_parser(s):
     m = DATE_RE.match(s.decode('gbk'))
-    date_delta = datetime.date(YEAR, int(m.group(1)), int(m.group(2))) - START_DATE
-    return date_delta.days
+    return date2int(datetime.date(YEAR, int(m.group(1)), int(m.group(2))))
 
 if __name__ == '__main__':
     raw_data = np.loadtxt(csv_data_path, dtype=int, delimiter=',', converters={3: date_parser}, skiprows=1)
