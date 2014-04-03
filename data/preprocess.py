11628201c5ed7ba61eabc914e978325ebdbb1681
leon-huang <leon@njuopen.com>
2014-04-03 20:46:27 +0800

    move path variables to global

--- data/preprocess.py
+++ data/preprocess.py
@@ -8,8 +8,10 @@ import os
 
 YEAR = 2013
 START_DATE = datetime.date(YEAR, 1, 1)
-
 DATE_RE = re.compile(u'(\d+)月(\d+)日')
+current_dir = os.path.split(os.path.abspath(__file__))[0]
+csv_data_path = os.path.join(current_dir, 't_alibaba_data.csv')
+raw_data_path = os.path.join(os.path.join(current_dir, 'raw_data.npy'))
 
 def date_parser(s):
     m = DATE_RE.match(s.decode('gbk'))
@@ -17,9 +19,5 @@ def date_parser(s):
     return date_delta.days
 
 if __name__ == '__main__':
-    current_dir = os.path.split(os.path.abspath(__file__))[0]
-    data_path = os.path.join(current_dir, 't_alibaba_data.csv')
-    raw_data = np.loadtxt(data_path, dtype=int, delimiter=',', converters={3: date_parser}, skiprows=1)
-
-    target_path = os.path.join(os.path.join(current_dir, 'raw_data.npy'))
-    np.save(target_path, raw_data)
+    raw_data = np.loadtxt(csv_data_path, dtype=int, delimiter=',', converters={3: date_parser}, skiprows=1)
+    np.save(raw_data_path, raw_data)
