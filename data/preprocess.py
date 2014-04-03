1a02112c78ffd7e4184660fdaf971cc6bb7503dc
leon-huang <leon@njuopen.com>
2014-04-03 20:54:36 +0800

    hide unecessary viarables

--- data/preprocess.py
+++ data/preprocess.py
@@ -7,16 +7,16 @@ import re
 import os
 
 YEAR = 2013
-START_DATE = datetime.date(YEAR, 1, 1)
-DATE_RE = re.compile(u'(\d+)月(\d+)日')
 current_dir = os.path.split(os.path.abspath(__file__))[0]
 csv_data_path = os.path.join(current_dir, 't_alibaba_data.csv')
 raw_data_path = os.path.join(os.path.join(current_dir, 'raw_data.npy'))
 
 def date2int(date):
+    START_DATE = datetime.date(YEAR, 1, 1)
     return (date - START_DATE).days
 
 def date_parser(s):
+    DATE_RE = re.compile(u'(\d+)月(\d+)日')
     m = DATE_RE.match(s.decode('gbk'))
     return date2int(datetime.date(YEAR, int(m.group(1)), int(m.group(2))))
 
