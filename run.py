ec10845c3a58efa0b9a7d8d84cf4908f9823d35c
leon-huang <leon@njuopen.com>
2014-04-06 09:47:39 +0800

    refactor: rename test* -> val*

--- run.py
+++ run.py
@@ -2,18 +2,18 @@
 # -*- coding: utf-8 -*-
 
 """
-A script to test model automatically or generate result file of the model.
+A script to validate model automatically or generate result file of the model.
 
 Usage
 -----
-    $ python run.py test model-sgm model-lr model-lfm
+    $ python run.py val model-sgm model-lr model-lfm
     $ python run.py gen model-sgm
 
 Arguments
 ---------
 
-1:  command, 'test' or 'gen'.
-    'test' - test the model
+1:  command, 'val' or 'gen'.
+    'val' - validate the model
     'gen'  - generate result file of the model
 
 2~: model name, i.e., the folder where corresponding pred.py lies
@@ -62,28 +62,28 @@ def dict_size(dictionary):
         size += len(dictionary[i])
     return size
 
-def f1(pred_result, test_result, all_userids):
+def f1(pred_result, val_result, all_userids):
     """
-    pred_result has the same format as test_result:
+    pred_result has the same format as val_result:
         key: user_id
         value: set of brand_id
     """
     pBrands = sum([len(pred_result[ui]) for ui in pred_result])
     hitBrands = 0
     for ui in all_userids:
-        if ui in pred_result and ui in test_result:
-            hitBrands += len(pred_result[ui].intersection(test_result[ui]))
-    bBrands = sum([len(test_result[ui]) for ui in test_result])
+        if ui in pred_result and ui in val_result:
+            hitBrands += len(pred_result[ui].intersection(val_result[ui]))
+    bBrands = sum([len(val_result[ui]) for ui in val_result])
     p = hitBrands * 1. / pBrands
     assert 0 < p <= 1
     r = hitBrands * 1. / bBrands
     assert 0 < r <= 1
     return p, r, 2.*p*r/(p+r)
 
-def test():
+def val():
     if len(sys.argv) < 3:
-        raise LookupError('Please specify model to test.')
-    test_cases = [
+        raise LookupError('Please specify model to val.')
+    val_cases = [
         (prep.date(5, 16), prep.date(6, 16), 'predicts 5/16 to 6/16', '5/16'),
         (prep.date(6, 16), prep.date(7, 16), 'predicts 6/16 to 7/16', '6/16'),
         (prep.date(7, 16), prep.date(8, 16), 'predicts 7/16 to 8/16', '7/16'),
@@ -98,22 +98,22 @@ def test():
         Ps = []
         Rs = []
         Fs = []
-        for TRAIN_DATE, TEST_DATE, DESC, _ in test_cases:
+        for TRAIN_DATE, TEST_DATE, DESC, _ in val_cases:
             model = pred.get_model()
             train_data = all_data[all_data[:, 3] < TRAIN_DATE]
             model.fit(train_data)
             predictions, _ = model.predict(TRAIN_DATE-1)
             pred_result = ndarray2dict(predictions)
-            test_data = all_data[np.logical_and(
+            val_data = all_data[np.logical_and(
                 all_data[:, 2] == 1,
                 np.logical_and(
                     all_data[:, 3] >= TRAIN_DATE,
                     all_data[:, 3] < TEST_DATE
                 )
             )]
-            test_result = ndarray2dict(test_data[:, :2])
+            val_result = ndarray2dict(val_data[:, :2])
             all_userids = np.unique(train_data[:, 0])
-            p, r, f = f1(pred_result, test_result, all_userids)
+            p, r, f = f1(pred_result, val_result, all_userids)
             Ps.append(p)
             Rs.append(r)
             Fs.append(f)
@@ -122,13 +122,13 @@ def test():
             print('Recall:    %f' % r)
             print('F1 Score:  %f' % f)
             print('# Pred:    %d' % dict_size(pred_result))
-            print('# Real:    %d' % dict_size(test_result))
+            print('# Real:    %d' % dict_size(val_result))
         pl.figure(model_name)
-        pl.plot(range(len(test_cases)), Ps)
-        pl.plot(range(len(test_cases)), Rs)
-        pl.plot(range(len(test_cases)), Fs)
+        pl.plot(range(len(val_cases)), Ps)
+        pl.plot(range(len(val_cases)), Rs)
+        pl.plot(range(len(val_cases)), Fs)
         pl.legend(('Precision', 'Recall', 'F1 Score'))
-        pl.xticks(range(len(test_cases)), [i[3] for i in test_cases])
+        pl.xticks(range(len(val_cases)), [i[3] for i in val_cases])
         pl.show()
         sys.path = sys_path
 
@@ -158,6 +158,6 @@ def gen():
     sys.path = sys_path
 
 if __name__ == '__main__':
-    if sys.argv[1] not in ['test', 'gen']:
+    if sys.argv[1] not in ['val', 'gen']:
         raise LookupError('Unknown command: %d' % sys.argv[1])
     eval('%s()' % sys.argv[1])
