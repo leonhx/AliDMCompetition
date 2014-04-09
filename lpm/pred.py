e0054d720bfb968b495d3a8413eda202779daf81
leon-huang <leon@njuopen.com>
2014-04-09 20:07:51 +0800

    remove useless function

--- lpm/pred.py
+++ lpm/pred.py
@@ -27,19 +27,3 @@ class LPM:
 def get_model():
     from sklearn.svm import LinearSVC
     return LR(model=LinearSVC(C=10, loss='l1'), alpha=0.7, degree=1)
-
-def sort_by(data, order=['user_id', 'brand_id', 'visit_datetime']):
-    actype = np.dtype({
-        'names': ['user_id', 'brand_id', 'type', 'visit_datetime'],
-        'formats': [np.long, np.long, np.int, np.int]
-        })
-    typed_data = np.zeros(len(data), dtype=actype)
-    for i in range(len(data)):
-        typed_data[i] = tuple(data[i])
-    typed_data.sort(order=order)
-    for i in range(len(typed_data)):
-        td = typed_data[i]
-        data[i][0] = td['user_id']
-        data[i][1] = td['brand_id']
-        data[i][2] = td['type']
-        data[i][3] = td['visit_datetime']
