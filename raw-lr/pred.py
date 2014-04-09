324c77ee6517d4d888b2a0f67d17e509b6228ea7
leon-huang <leon@njuopen.com>
2014-04-09 13:56:47 +0800

    raw-lr: skeleton

--- raw-lr/pred.py
+++ raw-lr/pred.py
@@ -17,19 +17,26 @@ class RawLR:
     model: An object which has interfaces `fit' and `predict'
 
     series: iterable of integers
-        For example, [1, 2, 3, 5, 7, 9, 12, 15, 18, 21, 25, 29] means
-        the time dimension should be splitted as 1, 2, 3-4, 5-6, 7-8, 9-11,
-        12-14, 15-17, 18-20, 21-24, 25-28, 29-
+        For example, [0, 1, 2, 3, 5, 7, 9, 12, 15, 18, 21, 25, 29] means
+        the time dimension should be splitted as 0, 1, 2, 3-4, 5-6, 7-8,
+        9-11, 12-14, 15-17, 18-20, 21-24, 25-28, 29-
     """
     def __init__(self, model, series):
         self.__model__ = model
+        assert len(series) > 0
         self.__series__ = series
     def fit(self, X):
-        pass
+        self.__X__ = X
     def predict(self, time_now):
+        self.__now__ = time_now
+        # ...
         return predictions, ratings
     def __extract__(self, X):
-        pass
+        for ui in np.unique(self.__X__[:, 0]):
+            u_data = self.__X__[self.__X__[:, 0] == ui]
+            for bi in np.unique(u_data[:, 1]):
+                ub_data = u_data[u_data[:, 1] == bi]
+                # ...
 
 def get_model():
     pass
